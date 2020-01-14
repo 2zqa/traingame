6f44d97e8654166554e4c2d4c8559ef3985b6646
Rutger Kok <rutger.kok@live.com>
2020-01-14 20:47:59 +0100

    Implement interpolation on tile placement
    This fixes quick mouse/finger movement making you skip tiles

--- TrainGame-Godot/Scenes/GameScreen.gd
+++ TrainGame-Godot/Scenes/GameScreen.gd
@@ -5,10 +5,14 @@ const GroundTileMap = preload("res://Scenes/GroundTileMap.gd")
 
 var selected_option  # GroundTile or ObjectTile
 
+var _previous_touch_pos: Dictionary
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
     self.selected_option = null
+    self._previous_touch_pos = Dictionary()
 
+# Places the current tile at the given canvas position
 func place(canvas_position: Vector2) -> void:
     if self.selected_option is GroundTile:
         var tilemap_grounds: GroundTileMap = $World/GroundTileMap
@@ -19,6 +23,30 @@ func place(canvas_position: Vector2) -> void:
         var ground_pos = tilemap_objects.viewport_pos_to_tile_pos(canvas_position)
         tilemap_objects.set_tile(ground_pos, self.selected_option)
 
+# Places the current tile in a line of all given canvas positions
+func place_interpolated(canvas_position1: Vector2, canvas_position2: Vector2) -> void:
+    var distance = canvas_position1.distance_to(canvas_position2)
+    var tilemap = null
+    if self.selected_option is GroundTile:
+        tilemap = $World/GroundTileMap
+    elif self.selected_option is ObjectTile:
+        tilemap = $World/ObjectsTileMap
+    
+    if tilemap != null:
+        var tile_pos1 = tilemap.viewport_pos_to_tile_pos(canvas_position1)
+        var tile_pos2 = tilemap.viewport_pos_to_tile_pos(canvas_position2)
+        var interpolation_steps = tile_pos1.distance_to(tile_pos2)
+        if interpolation_steps == 0:
+            # No interpolation necessary
+            tilemap.set_tile(canvas_position2, self.selected_option)
+        else:
+            # Interpolate
+            for i in range(interpolation_steps + 1):
+                var tile_pos = tile_pos1.linear_interpolate(tile_pos2, i / interpolation_steps)
+                tilemap.set_tile(tile_pos, self.selected_option)
+    else:
+        place(canvas_position2)  # Cannot interpolate for this type
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -26,16 +54,22 @@ func _process(delta: float) -> void:
          
     
 func _unhandled_input(event: InputEvent) -> void:
-    if Global.Mouse.is_left_released(event):
-        var tilemap_objects: ObjectsTileMap = $World/ObjectsTileMap
-        var tile = tilemap_objects.get_tile(tilemap_objects.mouse_event_to_tile_pos(event))
-        print(tile.name_id, " ", tile.rotation)
-    
     if (Global.Mouse.is_left_released(event)) \
             or event is InputEventScreenDrag\
             or (event is InputEventMouseMotion and event.get_button_mask() != 0):
-        place(event.position)
+        var pointer_id = Global.Mouse.get_pointer_id(event)
+        var previous_position = self._previous_touch_pos.get(pointer_id)
+        if previous_position != null:
+            self.place_interpolated(previous_position, event.position)
+        else:
+            self.place(event.position)
+        self._previous_touch_pos[pointer_id] = event.position
         self.get_tree().set_input_as_handled()
+    
+    if (event is InputEventScreenTouch and not event.is_pressed()) or \
+            Global.Mouse.is_left_released(event):
+        # Finger released, clear previous position
+        self._previous_touch_pos.erase(Global.Mouse.get_pointer_id(event))
     
 
 func _on_SideMenu_option_selected(selected_option):
--- TrainGame-Godot/Scripts/Mouse.gd
+++ TrainGame-Godot/Scripts/Mouse.gd
@@ -3,3 +3,10 @@ extends Node
 func is_left_released(event: InputEvent) -> bool:
     # Returns true if the given input event is an event where the left mouse button was released.
     return event is InputEventMouseButton and event.button_index == BUTTON_LEFT and not event.is_pressed()
+
+# Returns a pointer id for the given input event. If you touch the screen at two different locations,
+# both locations will have different pointer ids. If you're using a mouse, -1 will be returned.
+func get_pointer_id(event: InputEvent) -> int:
+    if event is InputEventScreenDrag or event is InputEventScreenTouch:
+        return event.index
+    return -1
