d329d8d7c8b511e4861acd24aada769a6b34691f
Rutger Kok <rutger.kok@live.com>
2020-02-02 10:56:44 +0100

    Refactor GroundTileSelectorButton to be a TextureButton

--- TrainGame-Godot/Scenes/SideMenu/GroundTileSelectorButton.gd
+++ TrainGame-Godot/Scenes/SideMenu/GroundTileSelectorButton.gd
@@ -9,28 +9,18 @@ func _ready() -> void:
     pass
 
 # Handles selection of tiles
-func _unhandled_input(event: InputEvent) -> void:  
-    if not Mouse.is_left_released(event):
-        return  # Ignore anything that is not a click release
-
-    var display: SingleTileDisplay = $Display
-    var local_mouse_pos = display.make_input_local(event).position
-    if local_mouse_pos.x < 0 or local_mouse_pos.y < 0\
-            or local_mouse_pos.x > display.size.x or local_mouse_pos.y > display.size.y:
-        return  # Clicked outside the button
+func _on_GroundTileSelectorButton_pressed() -> void:
 
     # Open popup
     var popup = $GroundTileSelectorPopup
     popup.popup()
 
     self.get_tree().set_input_as_handled()
-    
 
 
 func _on_GroundSelector_tile_selected(tile: GroundTile) -> void:
     # Update UI
-    var display : SingleTileDisplay = $Display
-    display.tile_id = tile.texture_id
+    self.texture_normal = tile.create_texture()
 
     # Forward
     emit_signal("tile_selected", tile)
--- TrainGame-Godot/Scripts/GroundTile.gd
+++ TrainGame-Godot/Scripts/GroundTile.gd
@@ -2,8 +2,15 @@ class_name GroundTile
 
 export(String) var name_id
 export(int) var texture_id
+const _tile_set = preload("res://TileSets/Ground.tres")
 
 func _init(name_id: String, texture_id: int):
     self.name_id = name_id
     self.texture_id = texture_id
 
+# Gets the texture offset in the tile set
+func create_texture() -> Texture:
+    var atlas = AtlasTexture.new()
+    atlas.atlas = self._tile_set.tile_get_texture(self.texture_id)
+    atlas.region = self._tile_set.tile_get_region(self.texture_id)
+    return atlas
