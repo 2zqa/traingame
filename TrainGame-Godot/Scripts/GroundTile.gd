455ef2e2f4caa7fc675d5aa81093ffdb52100f15
Rutger Kok <rutger.kok@live.com>
2020-02-02 16:19:03 +0100

    Use uppercase name for constant

--- TrainGame-Godot/Scripts/GroundTile.gd
+++ TrainGame-Godot/Scripts/GroundTile.gd
@@ -1,16 +1,17 @@
 class_name GroundTile
 
+const _TILE_SET := preload("res://TileSets/Ground.tres")
+
 export(String) var name_id
 export(int) var texture_id
-const _tile_set = preload("res://TileSets/Ground.tres")
 
 func _init(name_id: String, texture_id: int):
     self.name_id = name_id
     self.texture_id = texture_id
 
-# Gets the texture offset in the tile set
+# Creates the texture of this tile
 func create_texture() -> Texture:
     var atlas = AtlasTexture.new()
-    atlas.atlas = self._tile_set.tile_get_texture(self.texture_id)
-    atlas.region = self._tile_set.tile_get_region(self.texture_id)
+    atlas.atlas = _TILE_SET.tile_get_texture(self.texture_id)
+    atlas.region = _TILE_SET.tile_get_region(self.texture_id)
     return atlas
