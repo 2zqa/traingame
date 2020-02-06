f98ea9d0672a469527483a2f18526ee1cae1ae05
Rutger Kok <rutger.kok@live.com>
2020-02-06 21:57:55 +0100

    Add world renaming

--- TrainGame-Godot/Scenes/MainMenu/LoadWorldScreen.gd
+++ TrainGame-Godot/Scenes/MainMenu/LoadWorldScreen.gd
@@ -3,6 +3,7 @@ extends MarginContainer
 var _world_names: PoolStringArray
 var _world_files: PoolStringArray
 var _world_id: int = -1
+var _new_world_name: String = ""
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -46,7 +47,10 @@ func _on_DeleteButton_pressed() -> void:
     var world_index = self._world_id - 1
     if world_index < 0:
         return
-    var confirmation_dialog : ConfirmationDialog = $DeleteConfirmationDialog
+    var confirmation_dialog: ConfirmationDialog = $DeleteConfirmationDialog
+    var world_name = self._world_names[world_index]
+    confirmation_dialog.dialog_text = tr("You are about to delete the entire world named \"%s\", which cannot be undone.") % world_name \
+            + "\n\n" + tr("Are you sure you want to continue?")
     confirmation_dialog.popup_centered()
 
 
@@ -74,3 +78,33 @@ func _on_WorldSelector_item_selected(world_id: int) -> void:
         self._set_buttons_enabled(false)
     else:
         self._set_buttons_enabled(true)
+
+
+func _on_RenameButton_pressed() -> void:
+    var world_index = self._world_id - 1
+    if world_index < 0:
+        return
+
+    self._new_world_name = ""  # Clear recorded text
+    var rename_dialog: ConfirmationDialog = $RenameDialog
+    rename_dialog.popup_centered()
+
+
+func _on_WorldNameEdit_text_changed(new_text: String) -> void:
+    self._new_world_name = new_text
+
+
+# Called by the OK button in the rename dialog
+func _on_RenameDialog_confirmed() -> void:
+    if self._new_world_name.length() == 0:
+        $Container/FeedbackLabel.text = "Renaming failed: world name cannot be empty."
+        return
+
+    var world_index = self._world_id - 1
+    if world_index < 0:
+        return
+    
+    if FileIO.write_name(self._world_files[world_index], self._new_world_name) == OK:
+        self._initialize_world_list()
+        $Container/FeedbackLabel.text = tr("World renamed to \"%s\"!") % self._new_world_name
+        $Container/WorldSelector.select(self._world_id)  # And select correct option again
