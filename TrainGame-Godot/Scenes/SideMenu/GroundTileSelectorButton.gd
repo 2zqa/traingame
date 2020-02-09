1404ab1014abfaaf0d402860a49ba5d365f8303d
Rutger Kok <rutger.kok@live.com>
2020-02-09 19:22:30 +0100

    Remove unused import

--- TrainGame-Godot/Scenes/SideMenu/GroundTileSelectorButton.gd
+++ TrainGame-Godot/Scenes/SideMenu/GroundTileSelectorButton.gd
@@ -2,7 +2,6 @@ extends Control
 
 signal tile_selected  # Supplies one argument of type GroundTile
 
-const SingleTileDisplay = preload("res://Scenes/SingleTileDisplay.gd")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
