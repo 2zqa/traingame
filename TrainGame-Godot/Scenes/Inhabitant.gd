e439d1ee3098f15ad04ae572eb835cd3ef91ca18
Rutger Kok <rutger.kok@live.com>
2020-04-11 13:47:22 +0200

    Make inhabitant able to randomly change directions at intersections

--- TrainGame-Godot/Scenes/Inhabitant.gd
+++ TrainGame-Godot/Scenes/Inhabitant.gd
@@ -13,15 +13,7 @@ func _process(delta: float) -> void:
 
     if Global.paths.was_aligned(self._previous_position, self.position):
         self._teleport(Global.paths.snap_to_grid(self.position))
-        var tile_position = Global.paths.to_tile_pos(self.position)
-        var new_tile_position = tile_position + Direction.to_vector(self._direction)
-        if not Global.paths.can_walk_on(new_tile_position):
-            # Need to pick new direction
-            for direction in Direction.left_right_opposite(self._direction):
-                new_tile_position = tile_position + Direction.to_vector(direction)
-                if Global.paths.can_walk_on(new_tile_position):
-                    self._direction = direction
-                    break
+        self._reconsider_direction()
 
     # Move
     var direction_vector = Direction.to_vector(self._direction)
@@ -29,6 +21,31 @@ func _process(delta: float) -> void:
     self.position += direction_vector * self._speed * delta
     self._update_animation()
 
+# Requires the inhabitant to be snapped to the grid. Changes the current direction if blocked
+# or (by chance) at a T-intersection or a normal crossing.
+func _reconsider_direction() -> void:
+    var tile_position = Global.paths.to_tile_pos(self.position)
+    var forward_tile_position = tile_position + Direction.to_vector(self._direction)
+    var left_tile_position = tile_position + Direction.to_vector(Direction.left(self._direction))
+    var right_tile_position = tile_position + Direction.to_vector(Direction.right(self._direction))
+    var is_blocked = not Global.paths.can_walk_on(forward_tile_position)
+    if is_blocked or randf() < 0.33:
+        # Turn to left or right
+        var left_open = Global.paths.can_walk_on(left_tile_position)
+        var right_open = Global.paths.can_walk_on(right_tile_position)
+        if left_open and right_open and randf() > 0.5:
+            # Both left and right are possible. %0% chance to block off left
+            # to force a right turn
+            left_open = false 
+        if left_open:
+            self._direction = Direction.left(self._direction)
+            is_blocked = false
+        elif right_open:
+            self._direction = Direction.right(self._direction)
+            is_blocked = false
+    if is_blocked:
+        # Still blocked, reverse
+        self._direction = Direction.opposite(self._direction)
 
 func _update_animation() -> void:
     if self._direction == Direction.NORTH:
